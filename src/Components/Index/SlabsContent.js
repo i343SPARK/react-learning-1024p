f21d384ef7a073fdd4d315ba7eaef06cae187057
i343SPARK <pablosalas_@outlook.com>
2021-11-01 18:37:21 -0600

    react-index

--- src/Components/Index/SlabsContent.js
+++ src/Components/Index/SlabsContent.js
@@ -1,32 +1,44 @@
 import React from 'react'
 
+import start from '../../Assets/start.png'
+
+import { Link } from 'react-router-dom'
+
 export const SlabsContent = ({topic}) => {
     return (
         <div className="slab-counter">
             <h2>{topic.noSlab}</h2>
             <div className="slab-sect-a">
+            <Link className="linkSlab" to="/">
                 <article className="article-a1">
                     <h3 className="h3-slab">Contenido</h3>
                     <p className="p-slab">{topic.content}</p>
-                    <span></span>
+                    <img className="icon" src={start} alt="icon"/>
                 </article>
+            </Link>
+            <Link className="linkSlab" to="/">
                 <article className="article-b1">
                     <h3 className="h3-slab">Actividad A</h3>
                     <p className="p-slab">{topic.activityA}</p>
-                    <span></span>
+                    <img className="icon" src={start} alt="icon"/>
                 </article>
+            </Link>
             </div>
             <div className="slab-sect-b">
+            <Link className="linkSlab" to="/">
                 <article className="article-a2">
                     <h3 className="h3-slab">Actividad B</h3>
                     <p className="p-slab">{topic.activityB}</p>
-                    <span></span>
+                    <img className="icon" src={start} alt="icon"/>
                 </article>
+            </Link>
+            <Link className="linkSlab" to="/">
                 <article className="article-b2">
                     <h3 className="h3-slab">AUTOEVALUACIÓN</h3>
                     <p className="p-slab">{topic.test}</p>
-                    <span></span>
+                    <img className="icon" src={start} alt="icon"/>
                 </article>
+            </Link>
             </div>
         </div>
     )
