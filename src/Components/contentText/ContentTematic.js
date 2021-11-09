f0a79d9222fe8978859ad9ca16d1c95a7882cd36
i343SPARK <pablosalas_@outlook.com>
2021-11-09 08:37:24 -0600

    content-tematic

--- src/Components/contentText/ContentTematic.js
+++ src/Components/contentText/ContentTematic.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export function ContentTematic() {
     return (
-        <section>
-            
+        <section className="content-data">
+                <h2>contenido temático</h2>
+            <div className='timer-content'>
+                <span className='timer'></span>
+                <p className='p-time'>5 min.</p>
+            </div>
+            <section className='content-center'>
+                <div className='scroll-content'>
+                    <article className='info-describt'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</article>
+                </div>
+                <div>
+                    <Link className='content-buttom'><span></span> Contenido</Link>
+                    <Link className='video-buttom'><span></span> Video</Link>
+                </div>
+            </section>
+            <div>
+                <Link className='back-buttom'>Atrás</Link>
+                <Link className='next-buttom'>Listo</Link>
+            </div>
         </section>
     )
 }
--- src/Components/contentText/ContentTextScreen.js
+++ src/Components/contentText/ContentTextScreen.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { ContentDescribt } from './ContentDescribt'
 import { ContentHeader } from './ContentHeader'
+import { ContentTematic } from './ContentTematic'
 
 export function ContentTextScreen() {
     return (
         <>
             <ContentHeader/>
             <ContentDescribt/>
+            <ContentTematic/>
         </>
     )
 }
