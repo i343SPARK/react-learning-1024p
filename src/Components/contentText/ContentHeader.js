7fd7cc0ddb2609974bf4f8eefc6c71c90ec16599
i343SPARK <pablosalas_@outlook.com>
2021-11-09 07:43:34 -0600

    content-tematic

--- src/Components/contentText/ContentHeader.js
+++ src/Components/contentText/ContentHeader.js
@@ -2,12 +2,16 @@ import React from 'react'
 import header from '../../Assets/headercont.png'
 import cima from '../../Assets/CIMA.png'
 
+import { Link } from 'react-router-dom'
+
 export function ContentHeader() {
     return (
         <section className="cont-header">
             <img src={header} alt="header"/>
             <div className="header-inside">
+            <Link to="/">
                 <img src={cima} alt="cima"/>
+            </Link>
             </div>
         </section>
     )
--- src/Components/contentText/ContentTextScreen.js
+++ src/Components/contentText/ContentTextScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { ContentDescribt } from './ContentDescribt'
 import { ContentHeader } from './ContentHeader'
 
 export function ContentTextScreen() {
     return (
         <>
             <ContentHeader/>
+            <ContentDescribt/>
         </>
     )
 }
